c33ff8c365d77ff3d4789bd1f3d8fe3cd92af0ef
Dani Brito (@danibrito-dev) <dev.danibrito@gmail.com>
2025-06-12 21:37:13 -0300

    adicionado um load no carregamento dos detalhes de cada filme para evitar que as informações - detalhes- de outro filme, já apresentado anteriormente, sejam mostrado no novo carregamento

--- src/screens/Details/index.tsx
+++ src/screens/Details/index.tsx
@@ -1,6 +1,6 @@
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { useEffect, useState } from "react";
-import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { ActivityIndicator, Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { api } from "../services/api";
 import { BookmarkSimple, CalendarBlank, CaretLeft, Clock, Star } from "phosphor-react-native";
 
@@ -73,71 +73,76 @@ export function Details() {
           <BookmarkSimple color="#fff" size={32} weight="thin" />
         </TouchableOpacity>
       </View>
-
       
-      <View>
-         <Image
-            source={{
-              uri: `https://image.tmdb.org/t/p/w500${movieDetails?.backdrop_path}`,
-            }}
-            style={styles.detailsImage}
-          />
+        {loading && <ActivityIndicator size="large" color="#FFF" /> }
+        {!loading && <>
+        
+          <View>
+            <Image
+                source={{
+                  uri: `https://image.tmdb.org/t/p/w500${movieDetails?.backdrop_path}`,
+                }}
+                style={styles.detailsImage}
+              />
 
-         <Image
-            source={{
-              uri: `https://image.tmdb.org/t/p/w500${movieDetails?.poster_path}`,
-            }}
-            style={styles.detailsPosterImage}
-          />
-          <Text style={styles.titleMovie}>{movieDetails?.title}</Text>
-          <View style={styles.description}>
-            <View style={styles.descriptionGroup}>
-              <CalendarBlank color="#92929d" size={25} weight="thin" />
-              <Text style={styles.descriptionText} >
-                {getYear(movieDetails?.release_date)}
-              </Text>
-            </View>
-          
+            <Image
+                source={{
+                  uri: `https://image.tmdb.org/t/p/w500${movieDetails?.poster_path}`,
+                }}
+                style={styles.detailsPosterImage}
+              />
+              <Text style={styles.titleMovie}>{movieDetails?.title}</Text>
+              <View style={styles.description}>
+                <View style={styles.descriptionGroup}>
+                  <CalendarBlank color="#92929d" size={25} weight="thin" />
+                  <Text style={styles.descriptionText} >
+                    {getYear(movieDetails?.release_date)}
+                  </Text>
+                </View>
+              
 
-            <View style={styles.descriptionGroup}>
-              <Clock color="#92929d" size={25} weight="thin" />
-              <Text style={styles.descriptionText} >
-                {`${movieDetails?.runtime} minutos`}
-              </Text>
-            </View>
+                <View style={styles.descriptionGroup}>
+                  <Clock color="#92929d" size={25} weight="thin" />
+                  <Text style={styles.descriptionText} >
+                    {`${movieDetails?.runtime} minutos`}
+                  </Text>
+                </View>
 
-            <View style={styles.descriptionGroup}>
-              <Star
-              color={
-                movieDetails?.vote_average.toFixed(2) >= "7"
-                  ? "#FF8700"
-                  : "#92929D"
-              }
-              size={25}
-              weight={
-                movieDetails?.vote_average.toFixed(2) >= "7"
-                  ? "duotone"
-                  : "thin"
-              }
-            />
-              <Text 
-                style={[movieDetails?.vote_average.toFixed(2) >= "7" ? styles.descriptionText1 : styles.descriptionText]} 
-              >
-                {movieDetails?.vote_average.toFixed(1)}
-              </Text>            
-            </View>
+                <View style={styles.descriptionGroup}>
+                  <Star
+                  color={
+                    movieDetails?.vote_average.toFixed(2) >= "7"
+                      ? "#FF8700"
+                      : "#92929D"
+                  }
+                  size={25}
+                  weight={
+                    movieDetails?.vote_average.toFixed(2) >= "7"
+                      ? "duotone"
+                      : "thin"
+                  }
+                />
+                  <Text 
+                    style={[movieDetails?.vote_average.toFixed(2) >= "7" ? styles.descriptionText1 : styles.descriptionText]} 
+                  >
+                    {movieDetails?.vote_average.toFixed(1)}
+                  </Text>            
+                </View>
 
+              </View>
           </View>
-      </View>
 
-      <View style={styles.about}>
-        <Text style={styles.aboutText}>Sinopse</Text>
-        <Text style={styles.aboutText}>
-          {movieDetails?.overview === ""
-            ? "Ops! Parece que esse filme ainda não tem sinopse :-("
-            : movieDetails?.overview}
-        </Text>
-      </View>
+          <View style={styles.about}>
+            <Text style={styles.aboutText}>Sinopse</Text>
+            <Text style={styles.aboutText}>
+              {movieDetails?.overview === ""
+                ? "Ops! Parece que esse filme ainda não tem sinopse :-("
+                : movieDetails?.overview}
+            </Text>
+          </View>
+        
+        </> }
+
     </View>
   )
 }
